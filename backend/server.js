5fa1bc8458f54d5e94c2eb8bd5e2eeeb9f27f4c1
LeonMKD <leonzlatkovski056@gmail.com>
2026-06-20 19:39:49 +0200

    First commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+console.log(process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+
+  
+});
