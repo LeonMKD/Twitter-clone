311d95bb757043a2dc8252268efabdca9a161d83
LeonMKD <leonzlatkovski056@gmail.com>
2026-06-22 21:49:47 +0200

    Update backend controllers routes and notification model

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ import dns from "dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import express from "express";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
-
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,10 +29,9 @@ app.use(cookieParser());
 console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
-
-  
 });
